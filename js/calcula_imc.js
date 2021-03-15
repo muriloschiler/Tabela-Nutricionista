6c542caa3ea3fa948212721d81d27fa7ee638ea5
muriloschiler <muriloschiler.ls@gmail.com>
2021-03-15 17:10:39 -0300

    Valores do Form em um objeto

--- js/calcula_imc.js
+++ js/calcula_imc.js
@@ -2,11 +2,17 @@ const botaoadicionar = document.querySelector("#adicionar-paciente")
 
 botaoadicionar.addEventListener("click",function(Event){
 
+    Event.preventDefault();
+
     const formularioAdiciona = document.querySelector("#form-adiciona")
     
-    let inputNome = formularioAdiciona.elements.namedItem("nome").value
-    let inputPeso = formularioAdiciona.elements.namedItem("peso").value
-    let inputAltura = formularioAdiciona.elements.namedItem("altura").value
-    let inputGordura = formularioAdiciona.elements.namedItem("gordura").value
+    pacienteNovo={
+    inputNome : formularioAdiciona.elements.namedItem("nome").value,
+    inputPeso : formularioAdiciona.elements.namedItem("peso").value,
+    inputAltura : formularioAdiciona.elements.namedItem("altura").value,
+    inputGordura : formularioAdiciona.elements.namedItem("gordura").value,
+    }
 
+    console.log(pacienteNovo)
 })
+
