fe57158a5edbf3f35859719afdc374046b9c487f
muriloschiler <muriloschiler.ls@gmail.com>
2021-03-17 10:59:53 -0300

    Remocao console.log

--- js/filtro.js
+++ js/filtro.js
@@ -6,20 +6,21 @@ filtroTabela.addEventListener("input",function(){
 
 })
 
-function filtrarTabela(expressao){
-console.log(expressao)
-let regex = new RegExp(expressao,"i")
-let pacientes = document.querySelectorAll(".paciente")
 
-    pacientes.forEach(function(paciente){
-        tdNome = paciente.querySelector(".info-nome")
-        if ( !regex.test(tdNome.textContent) ){
-            paciente.classList.add("invisivel")
-        }
-        else{
-            paciente.classList.remove("invisivel")
-              
-        }
-    })
+
+function filtrarTabela(expressao){
+
+    let regex = new RegExp(expressao,"i")
+    let pacientes = document.querySelectorAll(".paciente")
+
+        pacientes.forEach(function(paciente){
+            tdNome = paciente.querySelector(".info-nome")
+            if ( !regex.test(tdNome.textContent) ){
+                paciente.classList.add("invisivel")
+            }
+            else{
+                paciente.classList.remove("invisivel")
+            }
+        })
 
 }
